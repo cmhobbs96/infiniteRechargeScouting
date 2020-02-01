05ce57c07b5f25e8c83cfd4e8188b2115e20655d
Melanie <watermelanie7@gmail.com>
2020-02-01 14:34:12 -0500

    updated welcome page, updated auto js, updated results page

--- www/app/auto/auto.controller.js
+++ www/app/auto/auto.controller.js
@@ -18,10 +18,13 @@
            
 
 			vm.matchProperties = {
-				switch: 0,
-				cubeDropped: 0,
-				scale: 0,
-                exchange: 0,
+				bottomPort: 0,
+				outerPort: 0,
+				innerPort: 0,
+				// switch: 0,
+				powerCellsDropped: 0,
+				// scale: 0,
+    //             exchange: 0,
 				autoRun: false,
 				placement: {
 					id: 0,
@@ -53,22 +56,28 @@
 				}
 			];
 
-			vm.increaseSwitch1 = increaseSwitch1;
+			// vm.increaseSwitch1 = increaseSwitch1;
 			
-			vm.decreaseSwitch1 = decreaseSwitch1;
+			// vm.decreaseSwitch1 = decreaseSwitch1;
 		
-			vm.increaseScale1 = increaseScale1;
+			// vm.increaseScale1 = increaseScale1;
 			
-			vm.decreaseScale1 = decreaseScale1;
-			vm.decreaseCubeDropped1 = decreaseCubeDropped1;
-			vm.increaseCubeDropped1 = increaseCubeDropped1;
+			// vm.decreaseScale1 = decreaseScale1;
+			vm.decreaseBottomPort1 = decreaseBottomPort1;
+			vm.increaseBottomPort1 = increaseBottomPort1;
+			vm.decreaseOuterPort1 = decreaseOuterPort1;
+			vm.increaseOuterPort1 = increaseOuterPort1;
+			vm.decreaseInnerPort1 = decreaseInnerPort1;
+			vm.increaseInnerPort1 = increaseInnerPort1;
+			vm.decreasePowerCellsDropped1 = decreasePowerCellsDropped1;
+			vm.increasePowerCellsDropped1 = increasePowerCellsDropped1;
 		
 			vm.toggleAutoRun = toggleAutoRun;
            
            
             vm.validStartingPos = validStartingPos;
-            vm.increaseExchange1 = increaseExchange1;
-            vm.decreaseExchange1 = decreaseExchange1;
+            // vm.increaseExchange1 = increaseExchange1;
+            // vm.decreaseExchange1 = decreaseExchange1;
 
             init();
 
@@ -76,58 +85,81 @@
 				console.log(vm.match);
 			}
 
-			function decreaseSwitch1() {
-				if(vm.matchProperties.switch - 1 >= 0) {
-					vm.matchProperties.switch -= 1;
+			// function decreaseSwitch1() {
+			// 	if(vm.matchProperties.switch - 1 >= 0) {
+			// 		vm.matchProperties.switch -= 1;
+			// 	}
+			// }
+
+			function decreaseBottomPort1() {
+				if (vm.matchProperties.bottomPort - 1 >= 0) {
+					vm.matchProperties.bottomPort -= 1;
 				}
 			}
 
-			function decreaseCubeDropped1() {
-				if(vm.matchProperties.cubeDropped - 1 >= 0) {
-					vm.matchProperties.cubeDropped -= 1;
+			function increaseBottomPort1() {
+				vm.matchProperties.bottomPort += 1;
+			}
+
+			function decreaseOuterPort1() {
+				if (vm.matchProperties.outerPort - 1 >= 0) {
+					vm.matchProperties.outerPort -= 1;
 				}
 			}
 
-			function increaseCubeDropped1() {
-				vm.matchProperties.cubeDropped += 1;
+			function increaseOuterPort1() {
+				vm.matchProperties.outerPort += 1;
+			}
+
+			function decreaseInnerPort1() {
+				if (vm.matchProperties.innerPort - 1 >= 0) {
+					vm.matchProperties.innerPort -= 1;
+				}
+			}
+
+			function increaseInnerPort1() {
+				vm.matchProperties.innerPort += 1;
+			}
+
+			function decreasePowerCellsDropped1() {
+				if(vm.matchProperties.powerCellsDropped - 1 >= 0) {
+					vm.matchProperties.powerCellsDropped -= 1;
+				}
+			}
+
+			function increasePowerCellsDropped1() {
+				vm.matchProperties.powerCellsDropped += 1;
 			}
 
 
-			function increaseSwitch1() {
-				vm.matchProperties.switch += 1;
-			}
+			// function increaseSwitch1() {
+			// 	vm.matchProperties.switch += 1;
+			// }
 
 		
 
-			function decreaseScale1() {
-				if(vm.matchProperties.scale - 1 >= 0) {
-					vm.matchProperties.scale -= 1;
-				}
-			}
+			// function decreaseScale1() {
+			// 	if(vm.matchProperties.scale - 1 >= 0) {
+			// 		vm.matchProperties.scale -= 1;
+			// 	}
+			// }
 
 			
 
-			function increaseScale1() {
-				vm.matchProperties.scale += 1;
-			}
+			// function increaseScale1() {
+			// 	vm.matchProperties.scale += 1;
+			// }
 
-            function decreaseExchange1() {
-				if(vm.matchProperties.exchange - 1 >= 0) {
-					vm.matchProperties.exchange-= 1;
-				}
+   //          function decreaseExchange1() {
+			// 	if(vm.matchProperties.exchange - 1 >= 0) {
+			// 		vm.matchProperties.exchange-= 1;
+			// 	}
                 
                 
-			}
-            function increaseExchange1() {
-				vm.matchProperties.exchange += 1;
-			}
-    
-          
-
-           
-            
-
-		
+			// }
+   //          function increaseExchange1() {
+			// 	vm.matchProperties.exchange += 1;
+			// }
 
 			function validStartingPos() {
             	var answer = true;
@@ -143,13 +175,13 @@
 
 			function submit() {
 				var autoScore = {
-                    //fuelpoints
-					switchPoints: vm.matchProperties.switch,
-                    //rotorpoints
-					scalePoints: vm.matchProperties.scale,
-                    //basepoints
-					exchangePoints: vm.matchProperties.exchange,
-                    dropped: vm.matchProperties.cubeDropped,
+					// switchPoints: vm.matchProperties.switch,
+					// scalePoints: vm.matchProperties.scale,
+					// exchangePoints: vm.matchProperties.exchange,
+					bottomPortTotal: vm.matchProperties.bottomPort,
+					outerPortTotal: vm.matchProperties.outerPort,
+					innerPortTotal: vm.matchProperties.innerPort,
+                    dropped: vm.matchProperties.powerCellsDropped,
 					total: 0,
 					autoRunPoints: vm.matchProperties.autoRun,
 					placement: vm.matchProperties.placement.value
@@ -159,7 +191,8 @@
 			
 				autoScore.total = autoScore.autoRunPoints;
             
-                autoScore.cubes = autoScore.switchPoints + autoScore.scalePoints + autoScore.exchangePoints + autoScore.dropped;
+                //autoScore.cubes = autoScore.switchPoints + autoScore.scalePoints + autoScore.exchangePoints + autoScore.dropped;
+                autoScore.powerCells = autoScore.bottomPortTotal + autoScore.outerPortTotal + autoScore.innerPortTotal  + autoScore.dropped;
 
 				vm.match.autoScore = autoScore;
 				MatchSvc.updateMatch(vm.match);
